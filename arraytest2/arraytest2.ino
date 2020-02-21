30d65528f0228a090ac63888a46c3a39820b27bf
xlinx <xxlinxx@gmail.com>
2020-02-21 16:48:33 +0800

    a little change with Harmonic means calc.

--- arraytest2/arraytest2.ino
+++ arraytest2/arraytest2.ino
@@ -19,23 +19,35 @@ void loop()
      data[datanumber] = analogRead(ainput);
      Serial.println(analogRead(ainput));
    }
-     if (inputNum >= datanumber)
-        {                                   //hmean= n/1/x1+1/x2+1/x3.....
-            int mean1 = 1/data[1]; //把array的內容變成1/x1
-            int mean2 = 1/data[2]; 
-            int mean3 = 1/data[3]; 
-            int mean4 = 1/data[4]; 
-            int mean5 = 1/data[5]; 
-            int mean6 = 1/data[6]; 
-            int mean7 = 1/data[7]; 
-            int mean8 = 1/data[8]; 
-            int mean9 = 1/data[9]; 
-            int mean10 = 1/data[10]; 
-           h_mean= datanumber/(mean1+mean2+mean3+mean4+mean5+mean6+mean7+mean8+mean9+mean10) ; //計算 h_mean
-            Serial.println("H_mean:");
-            Serial.println(h_mean);
-            
-    }     
+   float mean[sizeof(datanumber)];
+   float sum_of_mean=0;
+   for(int i=0;i<datanumber;i++){
+     mean[i]=1/data[i];
+     sum_of_mean+=mean[i];
+   }
+   h_mean= datanumber/sum_of_mean;
+
+   Serial.println("H_mean:");
+   Serial.println(h_mean);         
+   
+   
+    //  if (inputNum >= datanumber)
+    //     {                                   
+    //         //hmean= n/1/x1+1/x2+1/x3.....
+    //         int mean1 = 1/data[1]; //把array的內容變成1/x1
+    //         int mean2 = 1/data[2]; 
+    //         int mean3 = 1/data[3]; 
+    //         int mean4 = 1/data[4]; 
+    //         int mean5 = 1/data[5]; 
+    //         int mean6 = 1/data[6]; 
+    //         int mean7 = 1/data[7]; 
+    //         int mean8 = 1/data[8]; 
+    //         int mean9 = 1/data[9]; 
+    //         int mean10 = 1/data[10]; 
+    //        h_mean= datanumber/(mean1+mean2+mean3+mean4+mean5+mean6+mean7+mean8+mean9+mean10) ; //計算 h_mean
+    //         Serial.println("H_mean:");
+    //         Serial.println(h_mean);         
+    // }     
       
      
            delay(1000); //
